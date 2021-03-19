a3fcd6db0154a5f0b7c3bc5766467189460ecff7
RobPerez155 <robert.perez.psirho@gmail.com>
2021-03-19 18:00:18 -0400

    Updated solution w/ orig prompt and ascii ref

--- Code-Wars-Fun/Your-Order-Please.js
+++ Code-Wars-Fun/Your-Order-Please.js
@@ -1,3 +1,10 @@
+// Description:
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
 function order(words){ 
   const wordArray = words.split(' ').sort(function(a,b){ //.split turns our string into an array
     return a.match(/\d/) - b.match(/\d/); // What .sort() is doing is shown below
@@ -13,3 +20,7 @@ function order(words){
 
 order("is2 Thi1s T4est 3a")
 //"Thi1s is2 3a T4est")
+
+// Note: .sort() sorts numbers and letters as ascii number references(http://www.asciitable.com/)
+// [1,2,3,4,11,22,33,44].sort() = [1,11,2,22,3,33,4,44]
+//Thanks to wibb1 for extra clarification.
