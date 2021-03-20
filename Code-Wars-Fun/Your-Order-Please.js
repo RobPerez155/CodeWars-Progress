34381e9a648650f13a71e8fe287c27b4ebfda474
RobPerez155 <robert.perez.psirho@gmail.com>
2021-03-20 18:43:41 -0400

    Formatting update to Your Order Please

--- Code-Wars-Fun/Your-Order-Please.js
+++ Code-Wars-Fun/Your-Order-Please.js
@@ -11,9 +11,9 @@ function order(words){
   const wordArray = words.split(' ').sort(function(a,b){ //.split turns our string into an array
     return a.match(/\d/) - b.match(/\d/); // What .sort() is doing is shown below
     // 1 - 2 = -1 ---> 1 should go to the left of 2 -- 1, 2
-    // 4 - 1 = 3 ---> 4 should go to the right of 1 -- 1, 4, 2
-    // 4 - 2 = 2 ---> 4 should go to the right of 2 -- 1, 2, 4
-    // 3 - 2 = 1 ---> 3 should go to the right of 2 -- 1, 2, 3, 4
+    // 4 - 1 = 3 --->  4 should go to the right of 1 -- 1, 4, 2
+    // 4 - 2 = 2 --->  4 should go to the right of 2 -- 1, 2, 4
+    // 3 - 2 = 1 --->  3 should go to the right of 2 -- 1, 2, 3, 4
     // 3 - 4 = -1 ---> 3 should go to the left of 4 -- 1, 2, 3, 4
  })
   const wordString = wordArray.join(' '); // .join ' ' puts the array back into a string w/ spaces
